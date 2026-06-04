1814d612873083b0f3111df51a0ae1dd06d752f9
Priyanshu Kaushal <56739575+priyanshukaushal@users.noreply.github.com>
2026-06-04 19:19:34 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+        vector<int> freq(26,0);
+        int n = s.length();
+        for(int i = 0; i<n; i++) {
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+        }
+        for(int i = 0; i<26; i++) {
+            if(freq[i] != 0) return false;
+        }
+        return true;
+    }
+};
