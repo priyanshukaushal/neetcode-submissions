5798834f0ab6bcfa71910a456db391071febd868
Priyanshu Kaushal <56739575+priyanshukaushal@users.noreply.github.com>
2026-06-07 01:36:26 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string ans = "";
+        for(string s : strs) {
+            ans.append(to_string(s.length()));
+            ans.push_back('@');
+            ans.append(s);
+        }
+        return ans;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> ans;
+        int i = 0;
+        int n = s.length();
+
+        while(i<n) {
+            int j = i;
+            while(s[j] != '@') j++;
+            int l = stoi(s.substr(i,j-i));
+            i = j+1;
+            ans.push_back(s.substr(i,l));
+            i = i+l;
+        }
+        return ans;
+    }
+};
